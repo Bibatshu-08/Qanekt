36eb8bc73d8d78f8e0a3d33457ecfead9d9d8e9b
Bibatshu Thapa Chhetri <61724089+Bibatshu-08@users.noreply.github.com>
2020-08-29 07:52:34 +0545

    okay done lol with the about page

--- src/Layout/About.js
+++ src/Layout/About.js
@@ -32,7 +32,7 @@ export default function About(){
       </div>
       <div className="text">
       <h1>Who Are We?</h1>
-        <p>We are a group of young and ambitious tech enthusiasts driven to bring changes in the society through technology. It’s our cohesive belief that the problems we, or our previous generations faced, can be solved today by leveraging on the modern advancement of technology, which was the root behind the incentive of the idea. All four of us have felt somewhere in life that we could not explore more, innovate more, and collaborate more despite having constant curiosities and ideas bubbling in our heads. Now, although we cannot go back to the past and change it, we want to secure at least a platform that might help humanity to innovate more in the coming days. Yes, we are just passing by youngsters diverse in every aspect yet driven by the common goal to create an innovative and prosperous future.</p>
+        <p>We are a group of young and ambitious tech enthusiasts driven to bring changes in the society through technology. It’s our cohesive belief that the problems we, or our previous generations faced, can be solved today by leveraging on the modern advancement of technology, which was the root behind the incentive. All four of us have felt somewhere in life that we could not explore more, innovate more, and collaborate more despite having constant curiosities and ideas bubbling in our heads. Now, although we cannot go back to the past and change it, we want to secure at least a platform that might help humanity to innovate more in the coming days. Yes, we are just passing by youngsters diverse in every aspect yet driven by the common goal to create an innovative and prosperous future.</p>
       </div>
     </div>
   </motion.div>
